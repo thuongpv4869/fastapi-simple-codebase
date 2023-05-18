b9dda09fd79ec7f29f0ee530840d5360f1efff90
thuongpv <thuongpv@smartosc.com>
2023-05-18 11:20:19 +0700

    feat: add user profile model

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base_class import Base
 from .mixins import TimestampMixin
@@ -14,3 +17,11 @@ class User(TimestampMixin, Base):
     phone: Mapped[str] = mapped_column(unique=True, index=True)
     disabled: Mapped[bool] = mapped_column(default=False)
     password: Mapped[str] = mapped_column()
+
+
+class UserProfile(TimestampMixin, Base):
+    __tablename__ = "user_profile"  # noqa
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="user_profile")
+    avatar_path: Mapped[Optional[str]]
